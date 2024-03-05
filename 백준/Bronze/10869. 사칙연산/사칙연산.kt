226ed3244e849d0f1a679dcf3e0a24fcdea710ce
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-03-05 13:14:52 +0900

    [Bronze V] Title: 사칙연산, Time: 116 ms, Memory: 14540 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val array = readln().split(" ")
+    val a = array[0].toInt()
+    val b = array[1].toInt()
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+}
