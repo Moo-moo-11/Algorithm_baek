5bea1038c8f1e1e46dba24929c0cb3b1cf6635ec
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-03-20 15:09:05 +0900

    [level 1] Title: K번째수, Time: 35.25 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+        var answer = intArrayOf()
+        commands.forEach { answer += array.slice(it[0]-1..it[1]-1).sorted()[it[2]-1]}
+        return answer
+    }
+}
