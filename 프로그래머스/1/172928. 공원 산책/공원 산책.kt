4cbf73df04038155b172dcf055b8260a9e5a2df2
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-04-05 15:11:38 +0900

    [level 1] Title: 공원 산책, Time: 11.98 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.kt
@@ -0,0 +1,50 @@
+class Solution {
+    fun solution(park: Array<String>, routes: Array<String>): IntArray {
+        var answer = IntArray(2)
+        var obstacles = mutableListOf<Any>()
+        for (i in park.indices) {
+            if(park[i].contains('S')) {
+                answer[0] = i
+                answer[1] = park[i].indexOf('S')
+            }
+            if(park[i].contains('X')) {
+                for (j in park[i].indices) {
+                    if (park[i][j] == 'X') {
+                        obstacles.add(i to j)
+                    }
+                }
+            }
+        }
+        for (items in routes) {
+            if(items[0] == 'E') {
+                for (i in 1..items[2].digitToInt()) {
+                    if (answer[1]+items[2].digitToInt() > park[0].length-1) break
+                    if ((answer[0] to answer[1]+i) in obstacles) break
+                    if (i == items[2].digitToInt()) answer[1] = answer[1]+items[2].digitToInt()
+                }
+            }
+            if(items[0] == 'W') {
+                for (i in 1..items[2].digitToInt()) {
+                    if (answer[1]-items[2].digitToInt() < 0) break
+                    if ((answer[0] to answer[1]-i) in obstacles) break
+                    if (i == items[2].digitToInt()) answer[1] = answer[1]-items[2].digitToInt()
+                }
+            }
+            if(items[0] == 'S') {
+                for (i in 1..items[2].digitToInt()) {
+                    if (answer[0]+items[2].digitToInt() > park.size-1) break
+                    if ((answer[0]+i to answer[1]) in obstacles) break
+                    if (i == items[2].digitToInt()) answer[0] = answer[0]+items[2].digitToInt()
+                }
+            }
+            if(items[0] == 'N') {
+                for (i in 1..items[2].digitToInt()) {
+                    if (answer[0]-items[2].digitToInt() < 0) break
+                    if ((answer[0]-i to answer[1]) in obstacles) break
+                    if (i == items[2].digitToInt()) answer[0] = answer[0]-items[2].digitToInt()
+                }
+            }
+        }
+        return answer
+    }
+}
