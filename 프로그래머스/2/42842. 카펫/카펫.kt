80ebb5a293235abb3a2e1fc2733f1bbe31bdbfa7
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-04-11 14:37:13 +0900

    [level 2] Title: 카펫, Time: 0.20 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.kt
@@ -0,0 +1,20 @@
+import kotlin.math.*
+
+class Solution {
+    fun solution(brown: Int, yellow: Int): IntArray {
+        val total = brown + yellow
+        val answer = IntArray(2)
+        for (i in 3..round(sqrt(total.toDouble())).toInt()) {
+            if (total%i==0) {
+                val a = total/i
+                val b = i
+                if ((a-2)*(b-2) == yellow) {
+                    answer[0] = a
+                    answer[1] = b
+                    break
+                }
+            }
+        }
+        return answer
+    }
+}
