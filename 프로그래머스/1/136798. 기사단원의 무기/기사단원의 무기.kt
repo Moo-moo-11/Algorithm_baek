4226cab4f0e34442a16eff892dde627a019bdac4
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-04-12 22:09:24 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.68 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.kt
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.kt
@@ -4,15 +4,15 @@ class Solution {
     fun solution(number: Int, limit: Int, power: Int): Int {
         var answer: Int = 0
         for (i in 1..number) {
-            var divisor = 0
-            for (x in 1..floor(sqrt(i.toDouble())).toInt()) {
+            var divisor_numb = 0
+            for (x in 1..round(sqrt(i.toDouble())).toInt()) {
                 if(i%x == 0) {
-                    if(i/x == x) divisor++
-                    else divisor += 2
+                    if(i/x == x) divisor_numb++
+                    else divisor_numb += 2
                 }
             }
-            if(divisor > limit) answer += power
-            else answer += divisor
+            if(divisor_numb > limit) answer += power
+            else answer += divisor_numb
         }
         return answer
         }
