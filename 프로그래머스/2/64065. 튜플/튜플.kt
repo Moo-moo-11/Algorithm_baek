10848bfc444584e483595ba03116c446031571f0
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-05-19 21:06:05 +0900

    [level 2] Title: 튜플, Time: 19.63 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.kt
+++ 프로그래머스/2/64065. 튜플/튜플.kt
@@ -1,9 +1,8 @@
 class Solution {
     fun solution(s: String): IntArray {
-        val list = s.split(",")
+        val list = s.split(",").map {it.replace("{","").replace("}","").toInt()}
         val maps: HashMap<Int,Int> = hashMapOf()
-        for (word in list) {
-            val number = word.replace("{","").replace("}","").toInt()
+        for (number in list) {
             maps[number] = maps.getOrDefault(number, 0) + 1
         }
         val answer = IntArray(maps.size)
