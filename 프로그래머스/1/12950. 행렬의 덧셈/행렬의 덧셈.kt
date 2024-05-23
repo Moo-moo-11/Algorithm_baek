4fb2a3467cf3c3e27ff8023fdea732e18cd6580f
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-05-23 11:37:16 +0900

    [level 1] Title: 행렬의 덧셈, Time: 26.95 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.kt
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.kt
@@ -1,10 +1,10 @@
 class Solution {
     fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
-        var answer = arrayOf<IntArray>()
+        var answer = Array<IntArray>(arr1.size) { a -> intArrayOf()}
         for (i in 0..arr1.size-1) {
             val x = arr1[i]
             val y = arr2[i]
-            answer += x.zip(y){x,y -> x+y}.toIntArray()
+            answer[i] = x.zip(y){x,y -> x+y}.toIntArray()
         }
         return answer
     }
