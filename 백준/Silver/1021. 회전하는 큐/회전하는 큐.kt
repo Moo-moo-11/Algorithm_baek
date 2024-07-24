a875b5fe33169c5b01de68790196c55dfc9c29ac
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-07-24 22:26:34 +0900

    [Silver III] Title: 회전하는 큐, Time: 156 ms, Memory: 24428 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.kt
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.kt
@@ -5,23 +5,21 @@ fun main() {
     val list = readln().split(" ").map { it.toInt() }
 
     var count = 0
-    
+
     val deque = ArrayDeque(List<Int>(n) { it + 1 })
 
     for (num in list) {
-        
+
         val left = deque.indexOf(num)
-        
+
         val right = deque.size - left
-        
-        count += if (left < right) left else right
-        
-        if (left < right) repeat(left) { deque.addLast(deque.removeFirst()) }
-        else repeat(right) { deque.addFirst(deque.removeLast()) }
-        
+
+        if (left < right) repeat(left) { deque.addLast(deque.removeFirst()); count += 1 }
+        else repeat(right) { deque.addFirst(deque.removeLast()); count += 1 }
+
         deque.removeFirst()
     }
 
     println(count)
-    
+
 }
