405e187d3a5f59926485b080efe7d037e3522ae6
moomoo <122208574+Moo-moo-11@users.noreply.github.com>
2024-07-25 22:43:25 +0900

    [level 2] Title: 할인 행사, Time: 237.31 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.kt
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.kt
@@ -1,13 +1,14 @@
 class Solution {
     fun solution(want: Array<String>, number: IntArray, discount: Array<String>): Int {
         var answer :Int = 0
-
         val wantMap :HashMap<String,Int> = hashMapOf()
+        
         for (i in want.indices) {
             wantMap[want[i]] = number[i]
         }
 
         val maxDate = discount.size-9
+        
         for (x in 0..maxDate-1) {
             val discountMap :HashMap<String,Int> = hashMapOf()
             discount.slice(x..x+9).groupBy { it }.forEach { discountMap[it.key] = it.value.size }
